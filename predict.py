87b12d2e931ca94e557ba1ead79905a36c7e6d04
Marlen Zhumakov <mzhumakov@gmail.com>
2024-02-12 18:36:47 +0600

    Updated predict.py and added test_predict
    Added full functionality to predict.py
    Added test_predict to send requests and validate predict.py responses

---
+++ predict.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+import pickle
+import pandas as pd
+import numpy as np
+
+# загружаем модель
+model_file_path = "model\project_1_mastercard.sav"
+model = pickle.load(open(model_file_path, 'rb'))
+
+app = Flask('mastercard')
+
+# предсказываем закрывающую цену
+@app.route('/', methods=['POST'])
+def predict_close():
+    try:
+        customer = request.get_json()
+        input_dataframe = pd.DataFrame(customer, index=[0])
+        input_data = np.log1p(input_dataframe)
+        prediction = model.predict(input_data)
+        prediction_expm = round(*np.expm1(prediction), 2)
+        result = {'Closing Price' : prediction_expm}
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=9697)
